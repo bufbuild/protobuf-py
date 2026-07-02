3299d56b53fbd5c1fd9178622fa41484ee879a0c
Kolja <GitHub.Kolja@dfgh.net>
2026-07-02 10:22:48 +0200

    Add documentation for to/from_datetime to clarify timezone usage (#7)
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- src/protobuf/wkt/_mixin/_timestamp.py
+++ src/protobuf/wkt/_mixin/_timestamp.py
@@ -53,13 +53,43 @@ class TimestampMixin:
 
     @classmethod
     def from_datetime(cls: type[Self], dt: datetime, /) -> Self:
-        """Create from the given datetime."""
+        """Create from the given datetime.
+
+        Examples:
+            >>> from datetime import datetime, timezone, timedelta
+            >>> from protobuf.wkt import Timestamp
+            >>> Timestamp.from_datetime(datetime(1970, 1, 1, tzinfo=timezone.utc))
+            Timestamp()
+            >>> Timestamp.from_datetime(
+            ...     datetime(1970, 1, 1, 0, 0, 10, tzinfo=timezone.utc)
+            ... )
+            Timestamp(seconds=10)
+            >>> Timestamp.from_datetime(
+            ...     datetime(
+            ...         1970, 1, 1, tzinfo=timezone(timedelta(hours=5, minutes=30))
+            ...     )
+            ... )
+            Timestamp(seconds=-19800)
+        """
         return cls.from_nanos(
             ((dt.astimezone(timezone.utc) - EPOCH_DATETIME) // MICROSECOND_DELTA) * 1000
         )
 
     def to_datetime(self) -> datetime:
-        """Convert to a datetime."""
+        """Convert to a UTC datetime with timezone set.
+
+        To convert to a different timezone, use `datetime.astimezone()` on the result.
+
+        Examples:
+            >>> from datetime import timezone, timedelta
+            >>> from protobuf.wkt import Timestamp
+            >>> Timestamp(seconds=10).to_datetime()
+            datetime.datetime(1970, 1, 1, 0, 0, 10, tzinfo=datetime.timezone.utc)
+            >>> Timestamp(seconds=10).to_datetime().astimezone(
+            ...     timezone(timedelta(hours=5, minutes=30))
+            ... )
+            datetime.datetime(1970, 1, 1, 5, 30, 10, tzinfo=datetime.timezone(datetime.timedelta(seconds=19800)))
+        """
         return EPOCH_DATETIME + timedelta(
             seconds=self.seconds, microseconds=round(self.nanos / 1000)
         )
